ec0ac2d5d995e5b1fb13a78bb7da1b7ba0d84c88
CarlosEMFigueiredo <monteirocarlos166@gmail.com>
2025-08-20 19:12:54 -0300

    atualizacao atividade

--- AtividadeEmSala/src/AtividadeSala02.java
+++ AtividadeEmSala/src/AtividadeSala02.java
@@ -1,14 +1,17 @@
 public class AtividadeSala02 {
-    public static void main (String[] args){
-    }
-       String nome = "Carlos";
-       String sobrenome = "Figueiredo";
-       int idade =29;
-       double peso= 89;
-       double altura = 1.9;
-       double imc = 0;
+    public static void main(String[] args){
+        String nome = "Victor";
+        String sobrenome = "Silva";
+        int idade = 29;
+        double peso = 89;
+        double altura = 1.9;
+        double imc = 0;
+        System.out.println(nome + " " + sobrenome);
+        int anoNasc = 2025 - idade;
+        System.out.println("Ano de nascimento: " + anoNasc);
 
-        System.out.println(nome+ "" + sobrenome);
-        System.out.println("idade:" + idade);
+        String txtPesoAltura = "Peso: " + peso + "; Altura: " + altura;
+        System.out.println(txtPesoAltura);
+        imc = peso/(altura*altura);
+        System.out.println("Base calc IMC: " +peso+"/"+altura+"² = " + imc);
 
-}
